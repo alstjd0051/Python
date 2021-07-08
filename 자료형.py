c532eae4563c1397f5434880cf886d6388415e1e
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-07-08 19:46:20 +0900

    no commit message

---
+++ 자료형.py
@@ -0,0 +1,19 @@
+# 타입알아보기
+# a = 1
+# print(type(a))
+# b = 4.24e10
+# print(type(b))
+# c = 4.24e-10
+# print(type(c))
+
+
+# 사칙연산
+# a = 3
+# b = 4
+# print(a+b)
+# a=3
+# b=4
+# print(a*b)
+t1 = (1, 2, 'a', 'b')
+print(t1[0:2])
+print(t1)
